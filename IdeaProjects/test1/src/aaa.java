93118b77cc8887268c26c7a1eae551d727ee35a4
EthanLeslie <gaoyinghao@gaoyinghaodeMacBook-Pro.local>
2021-07-18 11:21:52 +0800

    codes

---
+++ IdeaProjects/test1/src/aaa.java
@@ -0,0 +1,7 @@
+public class aaa {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("world");
+        System.out.println("!");
+    }
+}
---
+++ VScodefiles/Fortest.java
@@ -0,0 +1,18 @@
+class Fortest {
+    public int v;
+    private String s;
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int  i = 0;
+        // if () {
+        for (int j = 0; j < args.length; j++) {
+            
+        }
+        // }
+        System.err.println("ByeBye");
+    }
+    public Fortest(){
+        v = 1;
+        
+    }
+}

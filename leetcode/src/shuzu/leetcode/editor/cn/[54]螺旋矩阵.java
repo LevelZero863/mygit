1a14aff439344335d024bd0ae7791a855a73583a
EthanLeslie <gaoyinghao@buzhidaosheideMacBook-Pro.local>
2021-07-31 17:28:26 +0800

    20210731PM

--- leetcode/src/shuzu/leetcode/editor/cn/[54]螺旋矩阵.java
+++ leetcode/src/shuzu/leetcode/editor/cn/[54]螺旋矩阵.java
@@ -2,7 +2,7 @@ package shuzu.leetcode.editor.cn;
 
 //给你一个 m 行 n 列的矩阵 matrix ，请按照 顺时针螺旋顺序 ，返回矩阵中的所有元素。 
 //
-// 
+//
 //
 // 示例 1： 
 //
@@ -31,10 +31,54 @@ package shuzu.leetcode.editor.cn;
 // Related Topics 数组 矩阵 模拟 
 // 👍 831 👎 0
 
+import java.util.ArrayList;
+import java.util.List;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-
+        List<Integer> ans = new ArrayList<>();
+        int m = matrix.length; //line
+        int n = matrix[0].length; //row
+        int flag = 1;//1-->right,2-->down,3-->left,4-->up
+        int bound1 = n-1;
+        int bound2 = m-1;
+        int bound3 = 0;
+        int bound4 = 1;
+        ans.add(matrix[0][0]);
+        int x = 0,y = 0;
+        while (true) {
+            if (flag == 1) {
+                if (y <= bound1) break;
+                for (int i = y + 1; i <= bound1; i++)
+                    ans.add(matrix[x][i]);
+                y = bound1;
+                bound1--;
+                flag = 2;
+            } else if (flag == 2) {
+                if (x <= bound2) break;
+                for (int i = x + 1; i <= bound2; i++)
+                    ans.add(matrix[i][y]);
+                x = bound2;
+                bound2--;
+                flag = 3;
+            } else if (flag == 3) {
+                if (y >= bound3) break;
+                for (int i = y - 1; i >= bound3 ; i--)
+                    ans.add(matrix[x][i]);
+                y = bound3;
+                bound3++;
+                flag = 4;
+            } else {
+                if (x >= bound4) break;
+                for (int i = x - 1; i >= bound4 ; i--)
+                    ans.add(matrix[x][y--]);
+                x = bound4;
+                bound4++;
+                flag = 1;
+            }
+        }
+        return ans;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
